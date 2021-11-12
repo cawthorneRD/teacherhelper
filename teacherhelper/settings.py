8c61d2284cd2a861fd918cd3fc54225eb6d99ac2
Bob <r.e.ducker@gmail.com>
2021-11-12 19:27:26 +0000

    bug fix database

--- teacherhelper/settings.py
+++ teacherhelper/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',)
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'eu09(ilk6@4sfdofb=b_2ht@vad*$ehh9-)3u_83+y%(+phh&=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
